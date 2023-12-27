8ddb7b69ca00b4a3156e9ab754a2c00c733c6139
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2023-12-27 17:32:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const chalk = require("chalk");
+const dotenv = require("dotenv");
+require("./config/connectDB.js");
+// const db = require("./models");
+
+const app = express();
+app.use(express.json());
+
+// let corsOptions = {
+//   origin: "http://localhost: 60001",
+// };
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+dotenv.config({ path: "./config/.env" });
+
+app.get("/", async (req, res) => {
+  res.send("Hello developer! API is working.");
+});
+
+// app.use("/api/v1",)
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, () => {
+  console.log(
+    chalk.bold.yellow(
+      `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+  );
+});
+
+// db.sequelize.sync().then((req) => {
+//   app.listen(PORT, () => {
+//     console.log(
+//       chalk.bold.yellow(
+//         `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+//       )
+//     );
+//   });
+// });
